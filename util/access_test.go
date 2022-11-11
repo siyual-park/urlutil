948e4c933e5552b407f074dd09f5a4ce65ad835d
siyual-park <siyual.bak@gmail.com>
2022-11-11 13:47:12 +0900

    test: Add more benchmark

--- util/access_test.go
+++ util/access_test.go
@@ -121,3 +121,110 @@ func TestSet(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkGet(b *testing.B) {
+	testCases := []struct {
+		name   string
+		source any
+		key    string
+	}{
+		{
+			name:   "map",
+			source: map[string]any{"k1": map[string]any{"k2": 1}},
+			key:    "k1.k2",
+		},
+		{
+			name:   "slice",
+			source: map[string]any{"k1": []map[string]any{{"k2": 1}}},
+			key:    "k1[0].k2",
+		},
+		{
+			name: "sync.Map",
+			source: map[string]any{"k1": func() *sync.Map {
+				m := sync.Map{}
+				m.Store("k2", 1)
+				return &m
+			}()},
+			key: "k1.k2",
+		},
+		{
+			name: "struct",
+			source: map[string]any{"k1": struct {
+				K2 int
+			}{
+				K2: 1,
+			}},
+			key: "k1.k2",
+		},
+	}
+
+	for _, tc := range testCases {
+		b.Run(tc.name, func(b *testing.B) {
+			_, ok := Get[any](tc.source, tc.key)
+			assert.True(b, ok)
+		})
+	}
+}
+
+func BenchmarkSet(b *testing.B) {
+	testCases := []struct {
+		name   string
+		source any
+		key    string
+		value  any
+	}{
+		{
+			name:   "map override",
+			source: map[string]any{"k1": map[string]any{"k2": 1}},
+			key:    "k1.k2",
+			value:  2,
+		},
+		{
+			name:   "map",
+			source: map[string]any{"k1": map[string]any{}},
+			key:    "k1.k2",
+			value:  2,
+		},
+		{
+			name:   "map ptr",
+			source: map[string]any{"k1": &map[string]any{}},
+			key:    "k1.k2",
+			value:  2,
+		},
+		{
+			name:   "slice",
+			source: map[string]any{"k1": []map[string]any{{"k2": 1}}},
+			key:    "k1[0].k2",
+			value:  2,
+		},
+		{
+			name:   "slice override",
+			source: map[string]any{"k1": []map[string]any{{"k2": 0}}},
+			key:    "k1[0]",
+			value:  map[string]any{"k2": 1},
+		},
+		{
+			name:   "sync.Map",
+			source: map[string]any{"k1": &sync.Map{}},
+			key:    "k1.k2",
+			value:  2,
+		},
+		{
+			name: "struct",
+			source: map[string]any{"k1": &struct {
+				K2 int
+			}{
+				K2: 1,
+			}},
+			key:   "k1.k2",
+			value: 2,
+		},
+	}
+
+	for _, tc := range testCases {
+		b.Run(tc.name, func(b *testing.B) {
+			ok := Set(tc.source, tc.key, tc.value)
+			assert.True(b, ok)
+		})
+	}
+}
--- util/copy_test.go
+++ util/copy_test.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strconv"
 	"sync"
 	"testing"
 )
@@ -35,5 +36,38 @@ func TestDeepCopy(t *testing.T) {
 	for _, tc := range testCases {
 		res := DeepCopy(tc.when)
 		assert.Equal(t, tc.when, res)
+	}
+}
+
+func BenchmarkDeepCopy(b *testing.B) {
+	testCases := []struct {
+		when any
+	}{
+		{
+			when: map[string]any{"k1": map[string]any{"k2": 1}},
+		},
+		{
+			when: map[string]any{"k1": []map[string]any{{"k2": 1}}},
+		},
+		{
+			when: map[string]any{"k1": func() *sync.Map {
+				m := sync.Map{}
+				m.Store("k2", 1)
+				return &m
+			}()},
+		},
+		{
+			when: map[string]any{"k1": struct {
+				K2 int
+			}{
+				K2: 1,
+			}},
+		},
+	}
+
+	for i, tc := range testCases {
+		b.Run(strconv.Itoa(i), func(b *testing.B) {
+			DeepCopy(tc.when)
+		})
 	}
 }
